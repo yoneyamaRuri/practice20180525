cbaf44ab7e920cf5445ee18b2acb81de5fe2f523
yoneyama <yoneyama@wize-net.co.jp>
2018-05-25 18:57:27 +0900

    no message

--- js/text.js
+++ js/text.js
@@ -1,18 +1,56 @@
-(function(){
+(function() {
 
-  //基本の配列
-  var cats = [  'taro', 'jiro'  ];
+  var cats = [
+    {
+      name: '1',
+      img: 'images/taro.jpg'
+    },
+    {
+      name: '2',
+      img: 'images/jiro.jpg'
+    },
+    {
+      name: '3',
+      img: 'images/saburo.jpg'
+    },
+    {
+      name: '4',
+      img: 'images/shiro.jpg'
+    },
+    {
+      name: '5',
+      img: 'images/goro.jpg'
+    },
+    {
+      name: '6',
+      img: 'images/rokuro.jpg'
+    }
+  ];
 
-  　$('.checkbtn').on('click', function(){
-      var inputNumber = $('input').val();
-      var i = inputNumber % 2;
-        if ( i === 1) {
-          console.log(cats[0]);
-        } else {
-          console.log(cats[1]);
-        }
-    });
 
 
+　$('#maincontent .checkbtn').on('click', function() {
+    var inputNumber = $('input').val();
+    var i = inputNumber % cats.length;
+
+    // var catName = '';
+    // if ( i === 1) {
+    //   catName = cats[0].name;
+    // } else {
+    //   catName = cats[1].name;
+    // }
+    //
+    // var catName = (i === 1) ? cats[0].name :  cats[1].name;
+
+    var catName = cats[i].name;
+
+    $('#maincontent .charname').text(catName);
+
+    var catImage = cats[i].img;
+
+    $('#maincontent img').attr({'src':catImage}),
+
+    $('#maincontent .result').show();
+  });
 
 })();
